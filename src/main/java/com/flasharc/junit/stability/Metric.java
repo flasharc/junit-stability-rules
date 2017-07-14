0ad48245b72a06564db5ec2d8f36acd7f34da767
abishek3876 <mailabishek@gmail.com>
2017-07-14 23:33:55 +0800

    Add CPU jiffy metric for android

--- src/main/java/com/flasharc/junit/stability/Metric.java
+++ src/main/java/com/flasharc/junit/stability/Metric.java
@@ -3,9 +3,9 @@ package com.flasharc.junit.stability;
 import java.util.List;
 
 public interface Metric {
-	void onRunStart();
-	void onRunFinish();
-	List<MetricResult> getResults();
+	void onRunStart() throws Exception;
+	void onRunFinish() throws Exception;
+	List<MetricResult> getResults() throws Exception;
 	
 	interface MetricResult {
 		String getMetricType();
