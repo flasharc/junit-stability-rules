b3683ed3aaa38bb5f9d79b9ff31b38369c0861d7
abishek3876 <mailabishek@gmail.com>
2017-07-14 22:11:29 +0800

    Forgot to add the new files

---
+++ src/main/java/com/flasharc/junit/stability/metrics/android/MemoryMetric.java
@@ -0,0 +1,102 @@
+package com.flasharc.junit.stability.metrics.android;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.flasharc.junit.stability.Metric;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
+import android.os.Debug.MemoryInfo;
+import android.support.test.InstrumentationRegistry;
+
+public class MemoryMetric implements Metric {
+	private final MemoryInfo memInfo;
+	private final Timer timer;
+	private TimerTask timerTask;
+	
+	private int maxMemoryUsage;
+	private int avgMemoryUsage;
+	private int count;
+	
+	public MemoryMetric() {
+		ActivityManager manager = (ActivityManager) InstrumentationRegistry.getTargetContext().getSystemService(Context.ACTIVITY_SERVICE);
+		int pid = -1;
+		for (RunningAppProcessInfo processInfo : manager.getRunningAppProcesses()) {
+			if (processInfo.processName.equals(InstrumentationRegistry.getTargetContext().getPackageName())) {
+				pid = processInfo.pid;
+				break;
+			}
+		}
+		
+		if (pid == -1) {
+			throw new IllegalStateException("Cannot retrieve the PID of the target app");
+		}
+		memInfo = manager.getProcessMemoryInfo(new int[] {pid})[0];
+		timer = new Timer(true);
+	}
+
+	@Override
+	public void onRunStart() {
+		timerTask = new TimerTask() {
+			@Override
+			public void run() {
+				int memory = memInfo.getTotalPss();
+				if (memory > maxMemoryUsage) {
+					maxMemoryUsage = memory;
+				}
+				count++;
+				avgMemoryUsage += (memory - avgMemoryUsage)/count;
+			}
+		};
+		timer.scheduleAtFixedRate(timerTask, 0, 1000);
+	}
+
+	@Override
+	public void onRunFinish() {
+		timerTask.cancel();
+	}
+
+	@Override
+	public List<MetricResult> getResults() {
+		double maxMem = maxMemoryUsage;
+		double avgMem = avgMemoryUsage;
+		
+		maxMemoryUsage = avgMemoryUsage = count = 0;
+		
+		return Arrays.<MetricResult>asList(
+				new MemoryMetricResult("Max Memory", maxMem),
+				new MemoryMetricResult("Avg Memory", avgMem));
+	}
+
+	private static class MemoryMetricResult implements MetricResult {
+		
+		private static final String METRIC_UNIT = "kB";
+		
+		private final String metricType;
+		private final double kiloBytes;
+		
+		private MemoryMetricResult(String metricType, double kiloBytes) {
+			this.kiloBytes = kiloBytes;
+			this.metricType = metricType;
+		}
+
+		@Override
+		public String getMetricType() {
+			return metricType;
+		}
+		
+		@Override
+		public String getMetricUnit() {
+			return METRIC_UNIT;
+		}
+
+		@Override
+		public double getMetricValue() {
+			return kiloBytes;
+		}
+	}
+}
---
+++ src/main/java/com/flasharc/junit/stability/metrics/android/NetworkMetric.java
@@ -0,0 +1,68 @@
+package com.flasharc.junit.stability.metrics.android;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.flasharc.junit.stability.Metric;
+import android.net.TrafficStats;
+import android.support.test.InstrumentationRegistry;
+
+public class NetworkMetric implements Metric {
+	private final int targetUid;
+	
+	private long startRxBytes;
+	private long startTxBytes;
+	private long endRxBytes;
+	private long endTxBytes;
+	
+	public NetworkMetric() {
+		targetUid = InstrumentationRegistry.getTargetContext().getApplicationInfo().uid;
+	}
+	
+	@Override
+	public void onRunStart() {
+		startRxBytes = TrafficStats.getUidRxBytes(targetUid);
+		startTxBytes = TrafficStats.getUidTxBytes(targetUid);
+	}
+
+	@Override
+	public void onRunFinish() {
+		endRxBytes = TrafficStats.getUidRxBytes(targetUid);
+		endTxBytes = TrafficStats.getUidTxBytes(targetUid);
+	}
+
+	@Override
+	public List<MetricResult> getResults() {
+		return Arrays.<MetricResult>asList(
+				new NetworkMetricResult("Data Transmitted", (endTxBytes - startTxBytes)),
+				new NetworkMetricResult("Data Received", (endRxBytes - startRxBytes)));
+	}
+
+	private static class NetworkMetricResult implements MetricResult {
+		
+		private static final String METRIC_UNIT = "bytes";
+		
+		private final String metricType;
+		private final double bytes;
+		
+		private NetworkMetricResult(String metricType, double bytes) {
+			this.bytes = bytes;
+			this.metricType = metricType;
+		}
+
+		@Override
+		public String getMetricType() {
+			return metricType;
+		}
+		
+		@Override
+		public String getMetricUnit() {
+			return METRIC_UNIT;
+		}
+
+		@Override
+		public double getMetricValue() {
+			return bytes;
+		}
+	}
+}
